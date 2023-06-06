9e62c622e20c2c2bcbcdded24dd7eff496be4421
DD1C74707 <gracevalley101@gmail.com>
2023-06-06 07:32:45 -0700

    Hot fix for task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(i, 10):
         if i != j and i != 8:
-            print(f"{i}{j}", end=", ")
-            if i == 8 and j == 9:
-                print(f"{i}{j}")
+            print("{}{}".format(i, j), end=", ")
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
