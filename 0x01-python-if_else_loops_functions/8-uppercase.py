4b0b95f8ed9585f60e6fe0c49dcdd8d69b74d79f
DD1C74707 <gracevalley101@gmail.com>
2023-06-06 08:31:09 -0700

    Hot fix task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,9 @@
 def uppercase(str):
     temp = []
     for i in range(len(str)):
-        if ord(str[i]) in range(ord('A'), ord('Z')+1):
-            temp.append(str[i])
-        else:
+        if ord(str[i]) in range(ord('a'), ord('z')+1):
             upper_case = ord(str[i]) - 32
             temp.append(chr(upper_case))
-    return ''.join(temp)
+        else:
+            temp.append(str[i])
+    print("{}".format(''.join(temp)))
