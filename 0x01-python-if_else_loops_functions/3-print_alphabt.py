3c69203fb203b0ff953dd4c0521d289700f3df8c
DD1C74707 <gracevalley101@gmail.com>
2023-06-06 05:41:49 -0700

    Hot fix task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if chr(c) == 'q' or  chr(c) == 'e':
+    if chr(c) == 'q' or chr(c) == 'e':
         pass
     else:
         print("{}".format(chr(c)), end="")
-
