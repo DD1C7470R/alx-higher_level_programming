2ef8f24040ea1a98da2c26df8b64b31ae9f12187
DD1C74707 <gracevalley101@gmail.com>
2023-06-06 06:00:14 -0700

    HOt fix for task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 100):
     if i != 99:
-        print(f"{i:02d}", end=", ")
+        print("{}".format('%02d' % i), end=", ")
     else:
-        print(f"{i:02d}")
+        print("{}".format(i))
