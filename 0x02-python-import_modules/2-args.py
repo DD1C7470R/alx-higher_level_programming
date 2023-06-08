fcbc8af91ad1f7a7804439b0b3dae75171e5f3ec
DD1C74707 <gracevalley101@gmail.com>
2023-06-08 06:26:28 -0700

    Hot fix for task 2 changed imp to use less lines of code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    arg_size = len(sys.argv[1:])
-    print("{} {}{}".format(arg_size, "argument" if arg_size == 1 else "arguments", ":" if arg_size > 0 else "."))
-    for arg in range(arg_size):
+    a_s = len(sys.argv[1:])
+    if a_s == 1:
+        print("{} {}{}".format(a_s, "argument", ":"))
+    else:
+        print("{} {}{}".format(a_s, "arguments", ":" if a_s > 0 else "."))
+    for arg in range(a_s):
         print("{}: {}".format(arg+1, sys.argv[arg + 1]))
