461f29e429901c00f29f5d264273c81e9ecbf112
DD1C74707 <gracevalley101@gmail.com>
2023-06-08 06:05:26 -0700

    Add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,6 @@
+import sys
+if __name__ == "__main__":
+    arg_size = len(sys.argv[1:])
+    print("{} {}{}".format(arg_size, "argument" if arg_size == 1 else "arguments", ":" if arg_size > 0 else "."))
+    for arg in range(arg_size):
+        print("{}: {}".format(arg+1, sys.argv[arg + 1]))
