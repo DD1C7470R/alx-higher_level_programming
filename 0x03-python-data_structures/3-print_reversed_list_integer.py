fe74e281e7ece7a3676e672478b14ed8715a59af
DD1C74707 <gracevalley101@gmail.com>
2023-06-12 12:15:52 -0700

    Add task 2

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list):
+    list_size = len(my_list)
+    while list_size > 0:
+        list_size -= 1
+        print("{:d}".format(my_list[list_size]))
