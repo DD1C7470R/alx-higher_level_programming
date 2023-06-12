b790e1b033246b27b51c08e51e1c1c954d8d2ff7
DD1C74707 <gracevalley101@gmail.com>
2023-06-12 12:24:12 -0700

    Add hot fix for task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def print_reversed_list_integer(my_list):
+def print_reversed_list_integer(my_list=[]):
     list_size = len(my_list)
     while list_size > 0:
         list_size -= 1
