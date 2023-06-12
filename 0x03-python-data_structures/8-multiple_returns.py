02918f7664da6fd2781c1055ac2785227ad4de8b
DD1C74707 <gracevalley101@gmail.com>
2023-06-12 12:47:10 -0700

    Task 8 Hot fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     if sentence == '':
-        return (0, len(sentence))
+        return (len(sentence), None)
     else:
         return (len(sentence), sentence[0])
