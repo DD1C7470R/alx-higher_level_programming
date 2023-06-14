3ab99bd458216bcc5b7bcacd4496d9b37728b757
DD1C74707 <gracevalley101@gmail.com>
2023-06-14 07:26:35 -0700

    Add task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    new_dict = sorted(a_dictionary)
+    for i, k in enumerate(new_dict):
+        print('{}: {}'.format(k, a_dictionary[k]))
