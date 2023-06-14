5f3066551f7281aed3145b28c743ed62c51decc1
DD1C74707 <gracevalley101@gmail.com>
2023-06-14 08:33:50 -0700

    Solve indentation for task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,19 +7,20 @@ def roman_to_int(roman_string):
         return 0
     sum_val = 0
     prev = ''
-    roman_numerals_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
-                            "D": 500, "CM": 900, "M": 1000}
+    roman_table = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
+            "D": 500, "M": 1000
+    }
     for k in roman_string:
-        if k in roman_numerals_table:
+        if k in roman_table:
             if prev != '':
-                if roman_numerals_table[k] > roman_numerals_table[prev]:
-                    sum_val = roman_numerals_table[k] -
-                    roman_numerals_table[prev]
+                if roman_table[k] > roman_table[prev]:
+                    sum_val = roman_table[k] - roman_table[prev]
                     prev = k
                 else:
-                    sum_val += roman_numerals_table[k]
+                    sum_val += roman_table[k]
                     prev = k
             else:
-                sum_val += roman_numerals_table[k]
+                sum_val += roman_table[k]
                 prev = k
     return sum_val
