25e83f1157c7880cdc9da6de217c8cc53239a7ce
DD1C74707 <gracevalley101@gmail.com>
2023-06-14 07:48:45 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,9 @@ def roman_to_int(roman_string):
         return 0
     if type(roman_string) != str:
         return 0
-    sum = 0;
-    roman_numerals_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "CM": 900, "M": 1000}
+    sum = 0
+    roman_numerals_table = {"I": 1, "V": 5, "X": 10, "L": 50,
+                            "C": 100, "D": 500, "CM": 900, "M": 1000}
     for k in roman_string:
         if k in roman_numerals_table:
             sum += roman_numerals_table[k]
