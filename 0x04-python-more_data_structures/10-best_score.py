e1f56e588c63f33ae1502a46bcbc738ce6fb0709
DD1C74707 <gracevalley101@gmail.com>
2023-06-14 07:36:12 -0700

    Hot fix for 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_val = 0;
+    max_val = 0
     key = ''
     for k, v in a_dictionary.items():
         if v > max_val:
