edc5ed526a16b020661f8b37ae379e85d2fd8485
DD1C74707 <gracevalley101@gmail.com>
2023-06-14 10:10:34 -0700

    Change task 12 implementation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if roman_string is None:
         return 0
     if type(roman_string) != str:
         return 0
     sum_val = 0
-    prev = ''
+    prev = 0
     roman_table = {
             "I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
             "D": 500, "M": 1000
     }
-    for k in roman_string:
+    for k in reversed(roman_string):
         if k in roman_table:
-            if prev != '':
-                if roman_table[k] > roman_table[prev]:
-                    sum_val = roman_table[k] - roman_table[prev]
-                    prev = k
-                else:
-                    sum_val += roman_table[k]
-                    prev = k
+            val = roman_table.get(k, 0)
+            if val >= prev:
+                sum_val += val
             else:
-                sum_val += roman_table[k]
-                prev = k
+                sum_val -= val
+        prev = val
     return sum_val
