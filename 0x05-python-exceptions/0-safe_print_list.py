8862466a9cd42955aeeaa817bff50877cc92f07f
DD1C74707 <gracevalley101@gmail.com>
2023-06-26 07:44:25 -0700

    Fix bare except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(my_list[idx]), end="")
             count += 1
         print("")
-    except:
+    except IndexError:
         print("")
     return count
-
