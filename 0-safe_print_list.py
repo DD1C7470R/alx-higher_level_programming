c405debf1b908a8278a4eb3f81257101eb904f04
DD1C74707 <gracevalley101@gmail.com>
2023-06-26 07:37:47 -0700

    Add task 0

---
+++ 0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for idx in range(0, x):
+            print("{:d}".format(my_list[idx]), end="")
+            count += 1
+        print("")
+    except:
+        print("")
+    return count
+
