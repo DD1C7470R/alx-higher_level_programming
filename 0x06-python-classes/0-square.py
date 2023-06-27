9caa6b2628cf4363b6fef0d40bc067b253f15e9d
DD1C74707 <gracevalley101@gmail.com>
2023-06-27 11:30:31 -0700

    Update docs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
 class Square:
-    """Empty class Square"""
+    """an empty class Square that defines a square.
+
+    Example:
+        $ python3 example_google.py
+
+    Attributes:
+        None
+
+    Todo:
+        * area
+        * my_print
+    """
     pass
