6df86558510b040d55aa928229d9f04b127b6938
DD1C74707 <gracevalley101@gmail.com>
2023-07-03 12:35:51 -0700

    Fixed pycode styling

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""This module defines an empty class 
+"""This module defines an empty class
 Rectangle that defines a rectangle
 """
 
 
 class Rectangle:
-    """This module defines an empty class 
+    """This module defines an empty class
     Rectangle that defines a rectangle
     """
     pass
