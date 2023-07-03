ed54cd8fa3a83491bf15ddeaae00035d74acc7a2
DD1C74707 <gracevalley101@gmail.com>
2023-07-03 12:41:21 -0700

    Add task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""This module defines an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """This module defines an empty class Rectangle that defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
