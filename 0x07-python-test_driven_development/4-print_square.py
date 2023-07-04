f37dd70c7c4be7091a3e1d86a25bd880e0cb328f
DD1C74707 <gracevalley101@gmail.com>
2023-07-04 04:57:18 -0700

    Add tdd task

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This module defines a function
+that prints a square with the character #.
+
+>>> print_square(2)
+##
+##
+"""
+
+
+def print_square(size):
+    """Defines a square function, takes in size"""
+    if size is None:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    rows = size
+    columns = size
+    for col in range(columns):
+        for row in range(rows):
+            print("#", end="")
+        print()
