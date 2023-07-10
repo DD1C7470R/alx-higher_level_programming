e65ef36cc5941665d9981f7b181542e392d1bc6d
DD1C74707 <gracevalley101@gmail.com>
2023-07-10 13:56:47 -0700

    Change implementation for task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,30 +15,10 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-    @property
-    def width(self):
-        """Get/set width values
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        self.__width = width
-
-    @property
-    def height(self):
-        """Get/set height values
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        self.__height = height
-
     def area(self):
         """Calculates the area of the rectangle
         """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.width, self.height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
