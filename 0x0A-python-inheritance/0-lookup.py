8870c9b218411f3d843f1c972ed99550a76608ef
DD1C74707 <gracevalley101@gmail.com>
2023-07-10 10:46:34 -0700

    Add task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,23 +1,13 @@
 #!usr/bin/python3
-"""Defines a function that 
-    returns the list of available attributes 
+"""Defines a function that
+    returns the list of available attributes
     and methods of an object
 """
 
 
 def lookup(obj):
-    """Defines a function that 
-    returns the list of available attributes 
+    """Defines a function that
+    returns the list of available attributes
     and methods of an object
     """
     return [dir(obj)]
-
-
-
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
