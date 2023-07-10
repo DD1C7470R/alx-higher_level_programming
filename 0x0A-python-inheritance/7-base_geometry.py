638aa6c7ab2f37c81c0ef270c647702e551fb409
DD1C74707 <gracevalley101@gmail.com>
2023-07-10 13:08:53 -0700

    Add task 7 hot fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Validates class Rectangle parameter values
         """
-        if not isinstance(value, int):
+        if not type(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
