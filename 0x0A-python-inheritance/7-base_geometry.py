2bfe645f1c4d0f0d5f14ff80e1c3777c7633cfe7
DD1C74707 <gracevalley101@gmail.com>
2023-07-10 12:55:24 -0700

    Add task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Defines an empty class BaseGeometry
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates class Rectangle parameter values
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        else:
+            pass
