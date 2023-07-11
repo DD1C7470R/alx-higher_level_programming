0811a43064ea2654d6e9abc1d029552d651e8938
DD1C74707 <gracevalley101@gmail.com>
2023-07-11 12:16:41 -0700

    Check for no filename

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,7 @@ def read_file(filename=""):
     """Define a function that
     reads a text file (UTF8) and prints it to stdout.
     """
+    if filename is None or len(filename) == 0:
+        return ""
     with open(filename, mode='r',  encoding="UTF8") as my_file:
         print(my_file.read())
