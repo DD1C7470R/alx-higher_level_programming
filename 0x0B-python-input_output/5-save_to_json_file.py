c320941372002caf98f3fec0311cb9870284ccc2
DD1C74707 <gracevalley101@gmail.com>
2023-07-11 12:53:39 -0700

    Remove white spaces in task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Defines a function  writes 
+""" Defines a function  writes
 an Object to a text file, using a JSON representation
 """
 
@@ -8,7 +8,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ Defines a function  writes an Object 
+    """ Defines a function  writes an Object
     to a text file, using a JSON representation
     """
 
