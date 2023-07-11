da4e2af049ee8440c50cb26ec814c2c0193281a0
DD1C74707 <gracevalley101@gmail.com>
2023-07-11 12:02:59 -0700

    Add task 0

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a function that
+    reads a text file (UTF8) and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """Define a function that
+    reads a text file (UTF8) and prints it to stdout.
+    """
+    with open(filename, mode='r',  encoding="UTF8") as my_file:
+        print(my_file.read())
