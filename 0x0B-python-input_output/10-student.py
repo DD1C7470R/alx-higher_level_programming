626eb9adf9ca17c8756db7ff1aff052b5864b85d
DD1C74707 <gracevalley101@gmail.com>
2023-07-11 13:30:40 -0700

    Add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Defines a function that returns the dictionary
+ description with simple data structure
+ (list, dictionary, string, integer and boolean)
+ for JSON serialization of an obje
+"""
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Defines a function that returns the dictionary
+        description with simple data structure
+        (list, dictionary, string, integer and boolean)
+        for JSON serialization of an obje
+        """
+        json = []
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if type(attr) == str:
+                    if hasattr(self, attr):
+                        json.append((attr, self.__getattribute__(attr)))
+        else:
+            for attr in self.__dict__:
+                if attr is not None:
+                    json.append((attr, self.__getattribute__(attr)))
+
+        return dict(json)
