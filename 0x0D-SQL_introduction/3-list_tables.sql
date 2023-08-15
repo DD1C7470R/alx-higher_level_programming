e0fd045afc9337d937fe0603f6c59e4241164317
DD1C74707 <gracevalley101@gmail.com>
2023-08-15 06:26:47 -0700

    Hot fix task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,2 @@
--- removes the databases hbtn_0c_0 on the server
-USE mysql;
-SELECT table_name FROM information_schema.tables WHERE table_schema = DATABASE();
+-- removes the databases hbtn_0c_0 on th
+SHOW TABLES;
