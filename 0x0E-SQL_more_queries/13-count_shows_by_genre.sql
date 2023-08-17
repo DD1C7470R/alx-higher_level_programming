6a179dbccfcc11ba28426fcb76047fe8d112e643
DD1C74707 <gracevalley101@gmail.com>
2023-08-16 18:24:55 -0700

    ADdd task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
--- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-       FROM tv_show_genres
-       INNER JOIN tv_genres
-       ON tv_genres.id = tv_show_genres.genre_id
-       GROUP BY tv_genres.name
-       ORDER BY number_shows DESC;
+-- This lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- where each record should display: tv_shows.title - tv_show_genres.genre_id and if a show doesn’t have a genre, display NULL
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
