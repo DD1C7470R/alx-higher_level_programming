dad1f20707d3a24516213ed37978c2bc27c587be
DD1C74707 <gracevalley101@gmail.com>
2023-08-16 18:31:03 -0700

    Addd tsk 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,9 @@
--- This lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
--- where each record should display: tv_shows.title - tv_show_genres.genre_id and if a show doesn’t have a genre, display NULL
-SELECT tv_genres.name FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+-- lists all genres the show dexter is listed under
+SELECT tv_shows.title 
+       FROM tv_shows
+       INNER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       	  INNER JOIN tv_genres
+	  ON tv_show_genres.genre_id = tv_genres.id
+	  WHERE tv_genres.name = 'Comedy'
+       ORDER BY tv_shows.title;
