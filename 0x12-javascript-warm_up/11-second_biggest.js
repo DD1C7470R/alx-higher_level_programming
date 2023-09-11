f6ed07cf61ea8ead53e0aa1c6d48095ba9aae665
DD1C747OR <gracevalley101@gmail.com>
2023-09-11 10:26:10 -0700

    Hot fix task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,24 @@
 #!/usr/bin/node
+// Get command-line arguments starting from the third argument
+const args = process.argv.slice(2);
 
-const myArray = [];
-for (let i = 2; i < process.argv.length; i++) {
-  if (Number(process.argv[i])) { myArray.push(Number(process.argv[i])); }
-}
-
-function secondLargest (myArrary) {
-  if (myArray.length <= 1) {
-    return (0);
-  } else {
-    myArray.sort();
-    return myArray[myArray.length - 2];
+function findSecondLargest (arr) {
+  if (arr.length < 2) {
+    return 0;
   }
+
+  // Convert the arguments to integers and filter out any non-integer arguments
+  const numbers = arr.map(Number).filter(Number.isInteger);
+
+  if (numbers.length < 2) {
+    return 0;
+  }
+
+  // Sort the numbers in descending order and return the second largest
+  const sortedNumbers = numbers.sort((a, b) => b - a);
+  return sortedNumbers[1];
 }
-console.log(secondLargest(myArray));
+
+// Call the function and log the result
+const result = findSecondLargest(args);
+console.log(result);
