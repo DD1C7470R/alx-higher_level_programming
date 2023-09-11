f810b122cff08c8261d17c7ffabc074d6c5ea02c
DD1C747OR <gracevalley101@gmail.com>
2023-09-11 10:07:32 -0700

    Add task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const myArray = [];
+for (let i = 2; i < process.argv.length; i++) {
+  myArray.push(Number(process.argv[i]));
+}
+
+function secondLargest (myArrary) {
+  if (myArray.length <= 1) {
+    return (0);
+  } else {
+    myArray.sort();
+    return myArray[myArray.length - 2];
+  }
+}
+console.log(secondLargest(myArray));
