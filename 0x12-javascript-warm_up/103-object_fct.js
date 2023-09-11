25774c765f4e7c82a49313771b395d5b2341aeca
DD1C747OR <gracevalley101@gmail.com>
2023-09-11 13:39:42 -0700

    fix task 107

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
   value: 12
 };
-console.log(myObject);
-myObject.incr = function () {
-  this.value++;
-};
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
 
+myObject.incr = function () {
+  myObject.value = myObject.incr + 1;
+};
+
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
