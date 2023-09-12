4dac4c0c609ddeaaa4ae5c06195e19289a28885b
DD1C747OR <gracevalley101@gmail.com>
2023-09-12 13:26:21 -0700

    Add task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rows = this.height;
+
+    while (rows > 0) {
+      let columns = this.width;
+      while (columns > 0) {
+        process.stdout.write('X');
+        columns -= 1;
+      }
+      rows -= 1;
+      console.log();
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+
+    return this;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+
+    return this;
+  }
+}
+
+module.exports = Rectangle;
