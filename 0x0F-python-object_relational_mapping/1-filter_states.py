e20ec6799d8c553f386a9be529e76191f4895e26
DD1C74707 <gracevalley101@gmail.com>
2023-09-15 02:44:10 -0700

    Hot fix task 0

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,13 +2,13 @@
 """Defines a method to get states"""
 
 import MySQLdb
+from sys import argv
 
-
-def getAllStates():
+if __name__ == '__main__':
     """Defines a method to get states"""
     conn = MySQLdb.connect(
-            host="localhost", port=3306, user="root", passwd="root",
-            db="hbtn_0e_0_usa", charset="utf8"
+            host="localhost", port=3306, user=argv[1], passwd=argv[2],
+            db=argv[3], charset="utf8"
         )
     cur = conn.cursor()
     cur.execute(
@@ -19,7 +19,3 @@ def getAllStates():
         print(row)
     cur.close()
     conn.close()
-
-
-if __name__ == '__main__':
-    getAllStates()
