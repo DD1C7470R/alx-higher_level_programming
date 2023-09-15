36979b3f232156a713498f0d7ac74c5f6f380140
DD1C74707 <gracevalley101@gmail.com>
2023-09-15 04:34:34 -0700

    Add task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,34 +2,28 @@
 """Defines a method to get states"""
 
 import MySQLdb
+from sys import argv
 import re
 
-def getAllStates(username, password, dbname, sp):
+if __name__ == '__main__':
     """Defines a method to get states"""
     conn = MySQLdb.connect(
-            host="localhost", port=3306, user=username, passwd=password,
-            db=dbname, charset="utf8"
+            host="localhost", port=3306, user=argv[1], passwd=argv[2],
+            db=argv[3], charset="utf8"
         )
     cur = conn.cursor()
-    query = (
-        "SELECT * FROM states WHERE name='{}'".format(sp)
-    )
-    cur.execute(query)
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+    pattern = "[!;@£$%^&*'\"]"
+    result = re.search(pattern, argv[4])
+    if result is not None and result.group():
+        pass
+    else:
+        cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+            states.id ASC".format(argv[4]).strip("'")
+        )
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            if row is not None:
+                print(row)
     cur.close()
     conn.close()
-
-
-if __name__ == '__main__':
-    import sys
-    args = sys.argv
-    username = args[1]
-    password = args[2]
-    dbname = args[3]
-    search_param = args[4]
-    pattern = r"[a-zA-Z]*"
-    result = re.match(pattern, search_param)
-    if result:
-        getAllStates(username, password, dbname, search_param)
