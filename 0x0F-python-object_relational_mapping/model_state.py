75902b4c13b8aededfcea3483c22cd35eae5eb46
DD1C74707 <gracevalley101@gmail.com>
2023-09-15 06:15:50 -0700

    Add task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Defines the state class"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String(128))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'{self.id} {self.name}'
