055cc8b5c95b5c2d078ff7ac0ce38d9632fb3f13
DD1C74707 <gracevalley101@gmail.com>
2023-09-15 02:17:38 -0700

    Hot fix task 0 output

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""Defines a method to get states"""
 
 import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY states.id ASC") # HERE I have to know SQL to grab all states in my database
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+
+def getAllStates():
+    """Defines a method to get states"""
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, user="root", passwd="root",
+            db="hbtn_0e_0_usa", charset="utf8"
+        )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name like 'N%' ORDER BY states.id ASC"
+    )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    getAllStates()
