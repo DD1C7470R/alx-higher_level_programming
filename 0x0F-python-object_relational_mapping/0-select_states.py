752f713a8e801d22c6eee326bb3d412a40595ba5
DD1C74707 <gracevalley101@gmail.com>
2023-09-15 02:38:11 -0700

    Fix task 0 implementation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,13 +2,13 @@
 """Defines a method to get states"""
 
 import MySQLdb
+from sys import argv
 
-
-def getAllStates(user, password, db):
+if __name__ == "__main__":
     """Defines a method to get states"""
     conn = MySQLdb.connect(
-            host="localhost", port=3306, user=user, passwd=password,
-            db="hbtn_0e_0_usa", charset="utf8"
+            host="localhost", port=3306, user=argv[1], passwd=argv[2],
+            db=argv[3], charset="utf8"
         )
     cur = conn.cursor()
     query = "SELECT * FROM states ORDER BY states.id ASC"
@@ -19,10 +19,3 @@ def getAllStates(user, password, db):
     cur.close()
     conn.close()
 
-
-if __name__ == '__main__':
-    import sys
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db = sys.argv[3]
-    getAllStates(user, password, db)
