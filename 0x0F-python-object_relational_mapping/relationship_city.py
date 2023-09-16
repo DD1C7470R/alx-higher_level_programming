b43c21d04a7dd24f4e50386e74722f021e3abd54
DD1C74707 <gracevalley101@gmail.com>
2023-09-16 03:13:53 -0700

    Add task 16

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,17 +11,9 @@ from sys import argv
 class City(Base):
     """
     Represents a city in the database.
-
-    Attributes:
-        __tablename__ (str): The name of the database table.
-        id (int): The primary key of the city record (auto-incremented).
-        name (str): The name of the city (maximum 128 characters).
-        state_id (int): The foreign key referencing the associated state's ID.
-        state (State): The relationship to the associated state
-            using the "State" class,
     """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    state = relationship("State", foreign_keys=state_id)
+    state = relationship("State")
