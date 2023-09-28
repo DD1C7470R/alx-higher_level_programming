beb522fe169bd47b39b53bcff34a66214f005d5a
DD1C7470R <gracevalley101@gmail.com>
2023-09-28 10:57:21 -0700

    Add task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Test function find_peak """
+
+
+def find_peak(my_list):
+    if len(my_list) == 0:
+        return None
+
+    peak_val = my_list[0]
+    for val in my_list:
+        if val > peak_val:
+            peak_val = val
+    return peak_val
