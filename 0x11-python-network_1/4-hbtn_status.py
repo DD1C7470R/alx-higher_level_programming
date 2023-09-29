cd527311418aee972b2dd0afae03c6fd03320f44
DD1C7470R <gracevalley101@gmail.com>
2023-09-29 06:07:32 -0700

    Add tsk 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+
+if __name__ == '__main__':
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    content = res
+    print("Body response:")
+    print("\t- type:", type(content.text))
+    print("\t- content:", content.text)
