7f87b1556cb06549f51645747dbc657cd25076d7
DD1C7470R <gracevalley101@gmail.com>
2023-09-29 05:49:39 -0700

    Add task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
+"""  script that takes in a URL, sends a request to the URL """
 # script that takes in a URL, sends a request to the URL and
-#displays the body of the response
+# displays the body of the response
 import urllib.request
-import urllib.parse
-import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    data = {}
-    data['email'] = sys.argv[2]
 
     try:
-	parsed_data = urllib.parse.urlencode(data).encode('ascii')
-        with urllib.request.urlopen(url, parsed_data) as response:
+        with urllib.request.urlopen(
+                'https://alx-intranet.hbtn.io/status'
+                ) as response:
             content = response.read()
-            print(content.decode('utf8'))
+            print("Body response:")
+            print("\t- type:", type(content))
+            print("\t- content:", content)
+            print("\t- utf8 content:", content.decode('utf8'))
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
