3c0bc5ee34059d7182b7ee22bd2faa8bedfd4d11
DD1C7470R <gracevalley101@gmail.com>
2023-09-29 10:43:06 -0700

    Add task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that takes your GitHub credentials and displays your id """
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    user = sys.argv[2]
+    url = f"https://api.github.com/repos/{user}/{repo}/commits"
+    commits = requests.get(url)
+    for ele in commits.json()[:10]:
+        name = ele.get('commit').get('author').get('name')
+        print(f"{ele.get('sha')}: {name}")
