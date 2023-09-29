7b68a5e8b80de361f1e14a6514f21ea1a372175a
DD1C7470R <gracevalley101@gmail.com>
2023-09-29 10:19:20 -0700

    Hot fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,13 +7,8 @@ import sys
 if __name__ == '__main__':
     headers = {}
     username = sys.argv[1]
-    headers['X-GitHub-Api-Version'] = "2022-11-28"
-    headers['Accept'] = "application/vnd.github+json"
     headers['Authorization'] = f"token {sys.argv[2]}"
 
     res = requests.get(f'https://api.github.com/users/{username}', headers)
-    if res.status_code == 200:
-        content = res.json()
-        print(content.get('id'))
-    else:
-        print('None')
+    content = res.json()
+    print(content.get('id'))
