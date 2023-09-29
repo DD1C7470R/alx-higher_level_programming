bb3ed2c8709572acf14f12866f05c63bb4e09ae3
DD1C7470R <gracevalley101@gmail.com>
2023-09-29 07:56:17 -0700

    Remove unexpected spaces around parameters

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == '__main__':
     body = {}
     body['email'] = sys.argv[2]
-    res = requests.post(sys.argv[1], data = body)
+    res = requests.post(sys.argv[1], data=body)
     content = res
     print(content.text)
