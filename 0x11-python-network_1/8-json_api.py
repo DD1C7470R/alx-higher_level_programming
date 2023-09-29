9759251f7fcefa3c3c12c2ebd752582084df7768
DD1C7470R <gracevalley101@gmail.com>
2023-09-29 08:54:58 -0700

    Add task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    body = {}
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    body['q'] = letter
+    res = requests.post('http://0.0.0.0:5000/search_user', data=body)
+    if res.headers.get('content-type') == 'application/json':
+        content = res.json()
+        if content:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
