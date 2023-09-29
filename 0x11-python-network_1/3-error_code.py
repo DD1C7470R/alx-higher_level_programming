87ddd605a0373ad85e2768aa8250250fdad885b9
DD1C7470R <gracevalley101@gmail.com>
2023-09-29 05:22:25 -0700

    Remove unused imports

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,6 @@
 # script that takes in a URL, sends a request to the URL and
 # displays the body of the response
 import urllib.request
-import urllib.parse
 import sys
 
 if __name__ == "__main__":
