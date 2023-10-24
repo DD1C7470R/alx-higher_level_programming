86b02448a35635eef381f3d85d042c01a8cc96ab
DD1C7470R <gracevalley101@gmail.com>
2023-10-24 08:12:50 -0700

    Add task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that reads and prints the content of a file.
+
+const fs = require('fs');
+const fileName = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileName, content, 'utf-8');
