d60132eab7ff755f1d4a26e863425b4ee7eaa1b5
DD1C7470R <gracevalley101@gmail.com>
2023-10-24 09:33:22 -0700

    Add task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// A script  that prints the number of movies where the character
+// “Wedge Antilles” is present.
+
+const request = require('request');
+const { writeFileSync } = require('fs');
+const url = process.argv[2];
+const fileName = process.argv[3];
+
+request(url, function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  }
+  const content = response.body;
+  writeFileSync(fileName, content, 'utf-8');
+});
