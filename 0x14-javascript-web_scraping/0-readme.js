856770b15a7e0283fbf432ae928dbc01862f5d8b
DD1C7470R <gracevalley101@gmail.com>
2023-10-24 08:06:43 -0700

    Hot fix semi standard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // A script that reads and prints the content of a file.
 
-const fs = require('fs')
-file_name = process.argv[2]
-fs.readFile(file_name, 'utf-8', (error, content) => {
-	if (error) {
-		console.log(error)
-		return;
-	}
-	console.log(content.toString())
-})
+const fs = require('fs');
+const fileName = process.argv[2];
+fs.readFile(fileName, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(content.toString());
+});
