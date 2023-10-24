e9775878b9e744b899711ae3b9ab70a7a70a7c51
DD1C7470R <gracevalley101@gmail.com>
2023-10-24 08:49:13 -0700

    Add task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// A script  that prints the number of movies where the character
+// “Wedge Antilles” is present.
+
+const request = require('request');
+const movieUrl = process.argv[2];
+let counter = 0;
+
+request(movieUrl, function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  }
+  const resultInjson = JSON.parse(response.body);
+
+  const data = resultInjson.results;
+
+  for (const obj of data) {
+    for (const character of obj.characters) {
+      if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        counter += 1;
+      }
+    }
+  }
+  console.log(counter);
+});
